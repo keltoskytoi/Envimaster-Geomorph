ddd45907e001c571f7a4d50240a13f5c41a5248f
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-06-29 15:09:17 +0200

    add example script to run fill sinks

---
+++ src/exmpl_run_fillsinks.R
@@ -0,0 +1,28 @@
+#############################################################################################
+###--- Setup Environment -------------------------------------------------------------------#
+                                  ###############################################           #
+# require libs for setup          #EEEE n   n v       v rrrr    m     m   ttttt #           #                  
+require(raster)                   #E    nn  n  v     v  r   r  m m   m m    t   #           #         
+require(envimaR)                  #EE   n n n   v   v   rrrr   m m   m m    t   #           #                
+require(link2GI)                  #E    n  nn    v v    r  r  m   m m   m   t   #           #             
+                                  #EEEE n   n     v     r   r m    m    m   t   #           #
+                                  ###############################################           #
+                                                                                            #
+# define needed libs and src folder                                                         #
+libs = c("link2GI") 
+pathdir = "repo/src/"
+
+#set root folder for uniPC or laptop                                                        #
+root_folder = alternativeEnvi(root_folder = "~/edu/Envimaster-Geomorph",                    #
+                              alt_env_id = "COMPUTERNAME",                                  #
+                              alt_env_value = "PCRZP",                                      #
+                              alt_env_root_folder = "F:/edu/Envimaster-Geomorph")           #
+#source environment script                                                                  #
+source(file.path(root_folder, paste0(pathdir,"001_setup_geomorph_withSAGA_v1.R")))                                                              
+###---------------------------------------------------------------------------------------###
+#############################################################################################
+dem <- raster::raster(file.path(envrmt$path_Cenith_V2, "exampl_dem.tif"))
+utm <- "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
+source(file.path(root_folder, paste0(pathdir,"saga_fillsinks.R"))) 
+cenith_fillsinks(dem=dem,output=envrmt$path_002_processed,tmp=envrmt$path_tmp,0,proj=utm)
