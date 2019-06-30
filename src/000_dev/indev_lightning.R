ea44377ab4b39c7f05f1adec22bb6fc9df06bc14
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-06-30 12:12:28 +0200

    new fucntion skyview
    SAGA Skyview wrapper

--- src/000_dev/indev_lightning.R
+++ src/000_dev/indev_lightning.R
@@ -12,19 +12,18 @@
 #' @param Mandatory if function: minslope - Minimum slope gradient to preserve from cell to cell;
 #' with a value of zero sinks are filled up to the spill elevation (which results in flat areas). Unit [Degree]
 #' @param Mandatory if function: proj - desired projection for output data, predefinition in var is recommended
+#' @param Mandatory if function: radius - 	The maximum search radius [map units]
 
-cenith_fillsinks <- function(dem,output,tmp,minslope,proj) {
+skyview <- function(dem,output,tmp,proj,radius) {
   raster::writeRaster(dem,filename=paste0(file.path(tmp),"/dem.sdat"),overwrite = TRUE,NAflag = 0)
-  
-  
   RSAGA::rsaga.geoprocessor(lib = "ta_lighting", module = 3,
-                            param = list(DEM = paste(envrmt$path_tmp,"/dem.sgrd", sep = ""), 
-                                         VISIBLE = paste(envrmt$path_tmp,"/visible.sgrd", sep = ""),
-                                         SVF = paste(envrmt$path_tmp,"/svf.sgrd", sep = ""),
-                                         SIMPLE= paste(envrmt$path_tmp,"/simple.sgrd", sep = ""),
-                                         TERRAIN = paste(envrmt$path_tmp,"/terrainsgrd", sep = ""),
-                                         DISTANCE= paste(envrmt$path_tmp,"/distance.sgrd", sep = ""),
-                                         RADIUS=100,
+                            param = list(DEM =     paste(tmp,"/dem.sgrd", sep = ""), 
+                                         VISIBLE = paste(tmp,"/vis.sgrd", sep = ""),
+                                         SVF =     paste(tmp,"/svf.sgrd", sep = ""),
+                                         SIMPLE=   paste(tmp,"/sim.sgrd", sep = ""),
+                                         TERRAIN = paste(tmp,"/ter.sgrd", sep = ""),
+                                         DISTANCE= paste(tmp,"/dis.sgrd", sep = ""),
+                                         RADIUS=radius,
                                          NDIRS =8,
                                          METHOD=0,
                                          DLEVEL=3
@@ -34,17 +33,23 @@ cenith_fillsinks <- function(dem,output,tmp,minslope,proj) {
                             show.output.on.console = TRUE, invisible = TRUE,
                             env = env)
   
-  
- # zum testen namen ändern.
-  # alle raster die produziert werden zusammenfassen und projezieren? stack?
+
   pr4 <- utm
-  filled_dem <- raster::raster(file.path(envrmt$path_tmp, "distance.sdat"))
-  proj4string(filled_dem) <- pr4
-  raster::writeRaster(filled_dem,filename=paste0(file.path(envrmt$path_tmp,"/distance.tif")),overwrite = TRUE,NAflag = 0)
-  fem <- raster::raster(file.path(envrmt$path_tmp,"/distance.tif"))
-  plot(fem)
-  som <- fem - dem
-  raster::writeRaster(som,filename=paste0(file.path(output),"/som.tif"),overwrite = TRUE,NAflag = 0)
+  vis <- raster::raster(file.path(tmp, "vis.sdat"))
+  svf <- raster::raster(file.path(tmp, "svf.sdat"))
+  sim <- raster::raster(file.path(tmp, "sim.sdat"))
+  ter <- raster::raster(file.path(tmp, "ter.sdat"))
+  dis <- raster::raster(file.path(tmp, "dis.sdat"))
+  proj4string(vis) <- pr4
+  proj4string(svf) <- pr4
+  proj4string(sim) <- pr4
+  proj4string(ter) <- pr4
+  proj4string(dis) <- pr4
+  raster::writeRaster(vis,filename=paste0(file.path(output,"/visible.tif")),overwrite = TRUE,NAflag = 0)
+  raster::writeRaster(svf,filename=paste0(file.path(output,"/svf.tif")),overwrite = TRUE,NAflag = 0)
+  raster::writeRaster(sim,filename=paste0(file.path(output,"/simple.tif")),overwrite = TRUE,NAflag = 0)
+  raster::writeRaster(ter,filename=paste0(file.path(output,"/terrain.tif")),overwrite = TRUE,NAflag = 0)
+  raster::writeRaster(dis,filename=paste0(file.path(output,"/distance.tif")),overwrite = TRUE,NAflag = 0)
   
 }
   
