6cdbc555915a05825b75add22b00f5ad984d975d
CSeimn <44396406+CSeimn@users.noreply.github.com>
2019-08-16 12:07:06 +0200

    Update Link_SAGA.R

--- src/Link_SAGA.R
+++ src/Link_SAGA.R
@@ -9,6 +9,7 @@ chosen_version <- as.numeric(readline(prompt = "Chose your preferred version num
 cat("#- Create SAGA environment -#", sep = "\n")
 SAGAenv <- RSAGA::rsaga.env(path = file.path(find_SAGA$binDir[chosen_version]))
 cat("#- SAGA-Environment created -#", sep = "\n")
+cat("!!!- Note: Set a variable 'env = SAGAenv' to every RSAGA function you execute, so your preferred SAGA version is used -!!!")
 rm(chosen_version)
 ##############################################################################################
 ##############################################################################################
