606e2b8a069fb2eda7c5ad13b3673de0c5ed4a61
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-09-04 12:44:37 +0200

    update test script

--- src/000_moving_window_test.R
+++ src/000_moving_window_test.R
@@ -30,24 +30,6 @@ source(file.path(root_folder, paste0(pathdir,"Cenith_V2/002_cenith_val_v2.R")))
 source(file.path(root_folder, paste0(pathdir,"Cenith_V2/dev_sf_cenith_val_a.R")))
 source(file.path(root_folder, paste0(pathdir,"Cenith_V2/dev_sf_cenith_val_b.R")))
 
-# load data
-dem <- raster::raster(file.path(root_folder, file.path(pathdir,".tif")))
-som <- raster::raster(file.path(root_folder, file.path(pathdir,".tif")))
-vp <-  rgdal::readOGR(file.path(root_folder, file.path(pathdir,".shp")))
-vp <- spTransform(vp,"+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
-compareCRS(som,vp) #check if projection is correct
-
-# invert dem
-dem_inv <- spatialEco::raster.invert(dem)
-
-# run several tests on som
-var <- cenith_val_v2(chm=som,f=1,a=c(),b=c(),h=c(),vp=vp)
-
-# run several tests on inverted dem
-var <- cenith_val_v2(chm=dem_inv,f=1,a=c(),b=c(),h=c(),vp=vp)
-
-### run Segmentation with CENITH V2
-
 #source CENITH V2
 source(file.path(root_folder, file.path(pathdir,"Cenith_V2/000_cenith_v2.R")))
 source(file.path(root_folder, file.path(pathdir,"Cenith_V2/cenith_tiles.R")))
@@ -56,9 +38,23 @@ source(file.path(root_folder, file.path(pathdir,"Cenith_V2/cenith_seg_tiles.R"))
 source(file.path(root_folder, file.path(pathdir,"Cenith_V2/cenith_merge.R")))
 source(file.path(root_folder, file.path(pathdir,"Cenith_V2/cenith_seg_v1.R"))) 
 
-# run Cenith on som
-test <- Cenith(chm=,h=,a=,b=)
-mapview(test2$tp)+som
-mapview(test2$polygon)+som
+# load data
+dem <- raster::raster(file.path(envrmt$path_Cenith_V2,"exampl_dem.tif"))
+som <- raster::raster(file.path(envrmt$path_Cenith_V2,"exmpl_som.tif"))
+vp <-  rgdal::readOGR(file.path(envrmt$path_Cenith_V2,"exmpl_vp.shp"))
+vp <- spTransform(vp,"+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+compareCRS(som,vp) #check if projection is correct
 
+# run several tests on som
+var <- cenith_val_v2(chm=som,f=1,a=c(1,2),b=c(0.1,0.9),h=c(0.1,0.5,0.9),vp=vp)
+
+var
+### run Segmentation with CENITH V2
+mapview(vp)+som
+# run Cenith on som
+test <- Cenith(chm=som,h=0.9,a=1,b=0.1)
+mapview(test$tp)+som
+mapview(test$polygon)+som
+
+test
 #end of script
