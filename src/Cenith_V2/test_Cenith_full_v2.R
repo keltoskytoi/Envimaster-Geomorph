2e188db62ec8c958fdc8287707fbd39b135ba37e
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-09-06 12:39:49 +0200

    update for merge polygons
    development for merging intersection polygons

--- src/Cenith_V2/test_Cenith_full_v2.R
+++ src/Cenith_V2/test_Cenith_full_v2.R
@@ -46,6 +46,9 @@ source(file.path(root_folder, file.path(pathdir,"Cenith_V2/sf_cenith_seg_v1.R"))
 chm <- raster::raster(file.path(envrmt$path_Cenith_V2,"exmpl_chm.tif"))
 som <- raster::raster(file.path(envrmt$path_Cenith_V2,"exmpl_som.tif"))
 vp <-  rgdal::readOGR(file.path(envrmt$path_Cenith_V2,"vp_wrongproj.shp"))
+crs(chm)
+crs(som)
+crs(vp)
 vp <- spTransform(vp,"+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
 compareCRS(som,vp) #check if projection is correct
 
@@ -73,6 +76,7 @@ identical(v2[,4],v1[,4])
 # run several tests on som
 var <- cenith_val_v2(chm=som,f=1,a=c(0.5,0.9),b=c(0.5,0.9),h=c(0.5,0.7),vp=vp)
 
+var <- v2
 # plot best hit rate
 maxrow <- var[which.max(var$hit),] # search max vale but rturn only 1 value
 maxhit <- maxrow$hit
@@ -87,9 +91,106 @@ test1 <- Cenith(chm=som,h=0.7,a=0.9,b=0.1)
 test2 <- Cenith(chm=som,h=0.7,a=0.5,b=0.5)
 test3 <- Cenith(chm=som,h=0.7,a=0.1,b=0.9)
 
+require(mapview)
+mapview(test1$polygon)+som
 # visual check for overlapping
 mapview(test1$polygon)+test2$polygon+test3$polygon
 mapview(test3$polygon)+som
+
+inter <-intersect(test1$polygon,test1$polygon)
+class(inter)
+mapview(inter)
+mapview(test1$polygon)
+identical(inter,test1$polygon)
+
+### test to merge
+library(sf)
+library(sp)
+library(rgeos)
+clusterSF <- function(sfpolys){
+  dmat = st_distance(sfpolys)
+  hc = hclust(as.dist(dmat), method="single")
+  groups = cutree(hc, h=0.5)
+  d = st_sf(
+    geom = do.call(c,
+                   lapply(1:max(groups), function(g){
+                     st_union(sfpolys[groups==g,])
+                   })
+    )
+  )
+  d$group = 1:nrow(d)
+  d
+}
+#################################
+#perape polygons
+polys <- test1$polygon
+poly_sf <- st_as_sf(polys)
+#poly_sf <- poly_sf_col[,5]
+class(poly_sf)
+poly_sf
+
+distancematrix <- st_distance(poly_sf)
+thresh <-grid::unit(1,"cm")
+hc = hclust(as.dist(distancematrix), method="single")
+
+hctests <- clusterSF(poly_sf,1)
+###
+set.seed(123)
+pols  = st_as_sf(
+  gBuffer(SpatialPoints(cbind(100*runif(20),100*runif(20))), width=12, byid=TRUE)
+)
+
+
+pc= clusterSF(poly_sf)
+plot(pols)
+plot(polscluster)
+mapview(pc)
+
+pc
+class(pols)
+class(poly_sf)
+
+class(polscluster)
+
+sp_cluster <- sf:::as_Spatial(pc)
+
+sp_cluster
+
+# test fall: es werden 2 polygone in einem crater gezeigt, die aneinander liegen.
+# Function läuft und merge beide, rdy to implenetate to validation algo.
+stat <- ForestTools::sp_summarise(vp, sp_cluster)
+
+stat[is.na(stat$TreeCount)] <- 0 # na to 0
+
+
+pkb <- sum(stat$TreeCount<1) # amount polygon without tree (miss)
+pb <- sum(stat$TreeCount==1) # amount polygon with exact 1 tree (hit)
+pmb <-sum(stat$TreeCount>1) # amount polygon with more than 1 tree (miss)
+
+hit = pb/length(stat$TreeCount) # calc hit ration in percent (amount of exact trees
+over = pkb/length(stat$TreeCount) #calc empty ration in percent (amount of polygon without trees)
+under = pmb/length(stat$TreeCount) 
+
+hit
+over
+under
+area <-area(sp_cluster)
+
+### check reuslts building
+result <- data.frame(matrix(nrow = 4, ncol = 4))
+j=3
+
+result[j, 1] <- a
+result[j, 2] <- b[j]
+result[j, 3] <- h
+result[j, 1] <- hit
+result[j, 5] <- ntree_vp
+result[j, 2] <- over
+result[j, 3] <- area[j]
+result[j, 4] <- under
+
+result
+
 #result all 3 test polygons overlap each other so the area is exactly the same
 
 identical(sum(test1$polygon$crownArea),
