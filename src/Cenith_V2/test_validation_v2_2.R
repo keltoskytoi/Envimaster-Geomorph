53816861c24ec234a09952bfcea369f997c35860
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-09-09 20:06:09 +0200

    delet v2.2

--- src/Cenith_V2/test_validation_v2_2.R
+++ src/Cenith_V2/test_validation_v2_2.R
@@ -73,29 +73,5 @@ maxhit <- maxrow$hit
 val21[which(val21$hit==maxhit),] 
 
 ################################################################################
-#source CENITH validation V2.2 validation a,b and h with more results and merging intersecting polygons
-source(file.path(root_folder, file.path(pathdir,"Cenith_V2/CENITH_val/CENITH_validation_V2.2/002_cenith_val_v2_2.R")))
-source(file.path(root_folder, file.path(pathdir,"Cenith_V2/CENITH_val/CENITH_validation_V2.2/sf_cenith_val_a_v2_2.R")))
-source(file.path(root_folder, file.path(pathdir,"Cenith_V2/CENITH_val/CENITH_validation_V2.2/sf_cenith_val_b_v2_1.R")))
-source(file.path(root_folder, file.path(pathdir,"Cenith_V2/CENITH_val/CENITH_validation_V2.2/sf_chmseg_clean.R")))
-source(file.path(root_folder, file.path(pathdir,"Cenith_V2/CENITH_val/CENITH_validation_V2.2/sf_ft_mcws_clean.R")))
-source(file.path(root_folder, file.path(pathdir,"Cenith_V2/CENITH_val/CENITH_validation_V2.2/sf_ft_vwf_clean.R")))
-
-#test CENITH validation V2.2
-val22 <- cenith_val_v2_2(chm=som,f=1,a=c(0.04,0.08),b=c(0.01,0.09),h=c(0.1,0.5,0.9),vp=vp_som,min=0,max=100)
-
-maxrow <- val22[which.max(val22$hit),] # search max vale but rturn only 1 value
-maxhit <- maxrow$hit
-val22[which(val22$hit==maxhit),] 
-
-#compare with clipping min and max
-val222 <- cenith_val_v2_2(chm=som,f=1,a=c(0.04,0.08),b=c(0.01,0.09),h=c(0.1,0.5,0.9),vp=vp_som,min=15,max=35)
-val22
-val222
-
-#test running on chm for treepos detection
-valtree <- cenith_val_v2_2(chm=chm,f=1,a=c(0.4,0.8),b=c(0.01,0.09),h=c(8,9,10),vp=vp_chm,min=0,max=100)
-# doenst work due to merging intersecting polygons
-################################################################################
 #end of script
  
